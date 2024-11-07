c6104f3f7bd58e2f21c33fb028cec3013cc1cc6e
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-11-07 15:47:11 +0800

    learning to use state and derived

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,15 +1,12 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	let count = $state(0)
+	let double = $derived(count * 2)
+</script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
-			<li><code class="code">/src/app.postcss</code> - app wide css</li>
-			<li>
-				<code class="code">/src/routes/+page.svelte</code> - this page, you can replace the contents
-			</li>
-		</ul>
+<div class="flex flex-col justify-center items-center h-screen">
+	<div class="flex flex-col justify-center items-center w-1/3">
+		<input type="number" class="input text-center w-24 mb-8" bind:value={count}>
+		<p>DOUBLED: {double}</p>
 	</div>
+	
 </div>
