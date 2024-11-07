fe2bdedea4202a2da0836bd2269bdd6aab8d453c
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-11-07 21:57:39 +0800

    added form and table to bucket page

--- my-app/src/routes/bucket/+page.server.ts
+++ my-app/src/routes/bucket/+page.server.ts
@@ -1,15 +1,56 @@
+import { z } from 'zod'
+import { superValidate, message } from 'sveltekit-superforms'
+import { zod } from 'sveltekit-superforms/adapters'
 import PocketBase from 'pocketbase'
 import { SECRET_PASSWORD, SECRET_EMAIL, SECRET_URL } from '$env/static/private'
+
+
+const schema = z.object({
+    Activity: z.string(),
+	Location: z.string(),
+	Category: z.string(),
+	LowEstimatedCost: z.number().optional(),
+	MediumEstimatedCost: z.number().optional(),
+	HighEstimatedCost: z.number().optional(),
+	Notes: z.string(),
+})
 
 export const load = async () => {
     const pb = new PocketBase(SECRET_URL)
     await pb.admins.authWithPassword(SECRET_EMAIL, SECRET_PASSWORD)
+    const form = await superValidate(zod(schema))
 
-    const records: Activity[] = await pb.collection('TRAVEL_ACTIVITIES').getFullList({
+    const records: Activity[] = await pb.collection("TRAVEL_ACTIVITIES").getFullList({
         sort: '-Location',
     })
 
     return {
+        form,
         records
+    }
+}
+
+export const actions = {
+    saveRecord: async ({ request }) => {
+        try {
+            const form = await superValidate(request, zod(schema))
+            // custom error checks
+
+            let newRecord: Activity = {
+                Activity: form.data.Activity,
+                Location: form.data.Location,
+                Category: form.data.Category,
+                LowEstimatedCost: form.data.LowEstimatedCost!,
+                MediumEstimatedCost: form.data.MediumEstimatedCost!,
+                HighEstimatedCost: form.data.HighEstimatedCost!,
+                Notes: form.data.Notes
+            }
+
+            const pb = new PocketBase(SECRET_URL)
+            await pb.admins.authWithPassword(SECRET_EMAIL, SECRET_PASSWORD)
+            await pb.collection("TRAVEL_ACTIVITIES").create(newRecord)
+        } catch (err) {
+            console.error("Error saving record:", err)
+        }
     }
 }
--- my-app/src/routes/bucket/+page.svelte
+++ my-app/src/routes/bucket/+page.svelte
@@ -1,5 +1,96 @@
 <script lang="ts">
+    import { superForm } from 'sveltekit-superforms'
+	import { Paginator, getToastStore } from '@skeletonlabs/skeleton'
+
     export let data
+
+    const { form, errors, message } = superForm(data.form)
+
+    // paginator logic
+	const source = [...data.records]
+	let paginationSettings = {
+		page: 0,
+		limit: 5,
+		size: source.length,
+		amounts: [5,10,30],
+	}
+	$: paginatedSource = source.slice(
+		paginationSettings.page * paginationSettings.limit,
+		paginationSettings.page * paginationSettings.limit + paginationSettings.limit
+	)
 </script>
 
-{JSON.stringify(data.records)}
+<div class="flex flex-col justify-center items-center h-screen">
+    <form class="w-1/4 flex flex-col mb-10 justify-center items-center gap-3" action="?/saveRecord" method="POST">
+        <div class="flex flex-col w-full">
+            {#if $errors.Activity}<span class="invalid italic m-0 p-0 text-xs text-error-400 ml-2 w-full">{$errors.Activity}</span>{/if}
+            <input name="Activity" type="text" bind:value={$form.Activity} class="input mt-0" placeholder="Activity">
+        </div>
+        
+        <div class="flex flex-col w-full">
+            {#if $errors.Location}<span class="invalid italic m-0 p-0 text-xs text-error-400 ml-2 w-full">{$errors.Location}</span>{/if}
+            <input name="Location" type="text" bind:value={$form.Location} class="input mt-0" placeholder="Location">
+        </div>
+
+        <div class="flex flex-col w-full">
+            {#if $errors.Category}<span class="invalid italic m-0 p-0 text-xs text-error-400 ml-2 w-full">{$errors.Category}</span>{/if}
+            <input name="Category" type="text" bind:value={$form.Category} class="input mt-0" placeholder="Category">
+        </div>
+
+        <div class="flex flex-col w-full">
+            {#if $errors.LowEstimatedCost}<span class="invalid italic m-0 p-0 text-xs text-error-400 ml-2 w-full">{$errors.LowEstimatedCost}</span>{/if}
+            <input name="LowEstimatedCost" type="text" bind:value={$form.LowEstimatedCost} class="input mt-0" placeholder="Low Estimated Cost">
+        </div>
+
+        <div class="flex flex-col w-full">
+            {#if $errors.MediumEstimatedCost}<span class="invalid italic m-0 p-0 text-xs text-error-400 ml-2 w-full">{$errors.MediumEstimatedCost}</span>{/if}
+            <input name="MediumEstimatedCost" type="text" bind:value={$form.MediumEstimatedCost} class="input mt-0" placeholder="Medium Estimated Cost">
+        </div>
+
+        <div class="flex flex-col w-full">
+            {#if $errors.HighEstimatedCost}<span class="invalid italic m-0 p-0 text-xs text-error-400 ml-2 w-full">{$errors.HighEstimatedCost}</span>{/if}
+            <input name="HighEstimatedCost" type="text" bind:value={$form.HighEstimatedCost} class="input mt-0" placeholder="High Estimated Cost">
+        </div>
+
+        <div class="flex flex-col w-full">
+            {#if $errors.Notes}<span class="invalid italic m-0 p-0 text-xs text-error-400 ml-2 w-full">{$errors.Notes}</span>{/if}
+            <input name="Notes" type="text" bind:value={$form.Notes} class="input mt-0" placeholder="Notes">
+        </div>
+
+        <button class="btn variant-ghost-primary w-1/2 rounded-md mt-4 text-wheat-500">Save Activity</button>
+    </form>
+
+<!-- TABLE -->
+<div class="table-container w-1/2">
+	<table class="table table-hover mb-4">
+		<thead>
+			<tr>
+				<th>Activity</th>
+				<th>Location</th>
+				<th>Catergory</th>
+				<th>Estimated Cost</th>
+				<th>Notes</th>
+			</tr>
+		</thead>
+		<tbody>
+			{#each paginatedSource as record, i}
+				<tr>
+					<td class="text-center">{record.Activity}</td>
+					<td class="text-center">{record.Location}</td>
+					<td class="text-center">{record.Category}</td>
+					<td class="text-center">{record.MediumEstimatedCost}</td>
+					<td class="text-center">{record.Notes}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+	<Paginator
+		bind:settings={paginationSettings}
+		showFirstLastButtons="{false}"
+		showPreviousNextButtons="{true}"
+		amountText="Records"
+	/>
+
+	<p class="w-full text-right underline italic mt-4"><a href="/stats">see stats?</a></p>
+</div>
+</div>
